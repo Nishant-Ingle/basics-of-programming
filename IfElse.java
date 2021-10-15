919564aa7cf71f97a0eca04d91f261ccd5a328e6
Gulabrao Ingle <57034878+gulabraoingle@users.noreply.github.com>
2021-10-15 16:38:09 +0530

    Add IfElse program

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+  public static void main(String[] args) {
+    // Try changing this to int number = 5
+    int number = 10;
+
+    if (number == 10) {
+      System.out.println("Value of number is 10");
+    else {
+      System.out.println("Value of number is not 10");
+    }
+  }
+}
